dbf1e573d2f696ed0206f87f502832b315e2a74d
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-05 12:24:33 +0530

    files added

--- json_loads.py
+++ json_loads.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from pathlib import Path
 
 data = Path("movies.json").read_text()
-# with open("movies.json") as file:
+# with open("movies.json", "r") as file:
 #     data = file.read()
 
 movies = json.loads(data)  # converts JSON file to Python readable format
