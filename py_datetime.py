bafad6ab6bad2e16efac9cd7dd20997f38720bb9
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-06 11:14:05 +0530

    files added

--- py_datetime.py
+++ py_datetime.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from time import time
 
-epic_time = time()  # returns the time from origin
+epic_time = time()  # returns the time from origin of OS
 print(epic_time)
+print("No of Years: ", epic_time/31536000)
 dt = datetime.now()  # returns the current timestamp
 print(dt)
 print(dt.strftime("%Y/%m/%d"))  # strftime conversts date to string format
