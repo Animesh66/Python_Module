708c91a2e43e3934d300ece51d30975dfc0ca287
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-06 13:20:36 +0530

    files added

--- py_datetime.py
+++ py_datetime.py
@@ -10,7 +10,7 @@ print(dt2.strftime("%Y/%m/%d"))  # strftime method  converts date to string form
 dt1 = datetime.strptime("2018-12-31", "%Y-%m-%d") + timedelta(days=1)
 print(dt1)  # strptime method converts string to date format
 print(datetime(2015, 12, 21, 10, 39, 57))
-print(f"{dt2.year}/{dt2.month}")
+print(f"{dt2.year}/{dt2.month}")  # Returns the
 duration = dt2 - dt1
 print(duration)
 print("Days: ", duration.days)
