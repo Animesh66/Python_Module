ef80b8d10ad36bf3c3e963179007b9a66a08a44b
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-06 11:43:20 +0530

    files added

--- py_datetime.py
+++ py_datetime.py
@@ -6,9 +6,9 @@ print(epic_time)
 print("No of Years: ", epic_time/31536000)
 dt2 = datetime.now()  # returns the current timestamp
 print(dt2)
-print(dt2.strftime("%Y/%m/%d"))  # strftime conversts date to string format
+print(dt2.strftime("%Y/%m/%d"))  # strftime method  converts date to string format
 dt1 = datetime.strptime("2018-12-31", "%Y-%m-%d") + timedelta(days=1)
-print(dt1)  # strptime converts string to date format
+print(dt1)  # strptime method converts string to date format
 print(datetime(2015, 12, 21, 10, 39, 57))
 print(f"{dt2.year}/{dt2.month}")
 duration = dt2 - dt1
