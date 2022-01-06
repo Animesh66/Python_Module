4814a15d33b0078aa1dd825397cdbf75cd84cabd
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-06 11:08:59 +0530

    files added

--- py_datetime.py
+++ py_datetime.py
@@ -4,4 +4,4 @@ from time import time
 dt = datetime.now()
 print(dt)
 print(dt.strftime("%Y/%m/%d"))  # strftime conversts date to string format
-print(datetime.strptime("2018/12/01","%Y/%m/%d"))
+print(datetime.strptime("2018-12-01", "%Y-%m-%d"))  # strptime converts string to date format
