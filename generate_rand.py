29cea29a02decd9da17c6e1f2787a04619b7501d
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-06 16:53:06 +0530

    files added

--- generate_rand.py
+++ generate_rand.py
@@ -10,5 +10,5 @@ print(random.choices(my_list, k=3))  # Randomly picks THREE(k) item from "my_lis
 print(random.choices(string.ascii_letters + string.digits + "@!_", k=10))
 generate_password = "".join(random.choices(string.ascii_letters + string.digits + "@!_", k=10))
 print(generate_password)
-print(random.shuffle(new_list))  # Randomly change the element position
+print(random.shuffle(new_list))  # Randomly changes the element position
 print("".join(['a','b','c','d','e','f']))
