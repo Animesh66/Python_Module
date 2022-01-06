39106d861bdb023f2fc7c61b9769b8b55fc51540
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-06 12:15:30 +0530

    files added

--- generate_rand.py
+++ generate_rand.py
@@ -6,5 +6,5 @@ print(random.random())  # Generates a random number between 0 and 1
 print(random.randint(1, 10))  # Generate a random  integer between 1 and 10
 print(random.choice(my_list))  # Randomly picks ONE item from my_list
 print(random.choices(my_list,k=3))  # Randomly picks THREE item from my_list
-generate_password = "".join(random.choices(string.ascii_letters + string.digits + string.ascii_uppercase, k=10))
+generate_password = "".join(random.choices(string.ascii_letters + string.digits + "@!_", k=10))
 print(generate_password)
