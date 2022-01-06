3acb08188200886b774cd17e0b0527531c1dd69e
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-06 16:52:04 +0530

    files added

--- generate_rand.py
+++ generate_rand.py
@@ -4,9 +4,9 @@ import string
 my_list = [1, 2, 3, "a", 2.0, 2.7, "stamp"]
 new_list = [1, 2, 3, 4]
 print(random.random())  # Generates a random number between 0 and 1
-print(random.randint(1, 10))  # Generate a random  integer between 1 and 10
-print(random.choice(my_list))  # Randomly picks ONE item from my_list
-print(random.choices(my_list, k=3))  # Randomly picks THREE item from my_list
+print(random.randint(1, 10))  # Generates a random  integer between 1 and 10
+print(random.choice(my_list))  # Randomly picks ONE item from "my_list"
+print(random.choices(my_list, k=3))  # Randomly picks THREE(k) item from "my_list"
 print(random.choices(string.ascii_letters + string.digits + "@!_", k=10))
 generate_password = "".join(random.choices(string.ascii_letters + string.digits + "@!_", k=10))
 print(generate_password)
