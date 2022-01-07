d733f2eb91d1af2c955d9a566faf675021cbaad4
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-07 19:50:28 +0530

    files added

--- my_database.py
+++ my_database.py
@@ -1,3 +1,7 @@
 from mysql.connector import connect
 
-mydb = connect()
+mydb = connect(host="",user="",password="",database="")
+
+my_cursor = mydb.cursor()
+my_cursor.execute("write MySql query")
+my_result= my_cursor.fetchone()
