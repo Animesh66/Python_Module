896dc5a7fe18dadabf3fcf1ac937e57ff9836f0e
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-10 13:00:58 +0530

    files added

--- my_database.py
+++ my_database.py
@@ -1,8 +1,18 @@
 from mysql.connector import connect
 
-mydb = connect(host="", user="", password="", database="")
+mydb = connect(host="127.0.0.1", user="root", password="nitaI9093@", database="sql_store")
+
+my_query = """
+SELECT * 
+FROM customers
+WHERE points BETWEEN 1000 AND 3000
+ORDER BY points DESC
+LIMIT 1,1;
+"""
 
 my_cursor = mydb.cursor()
-my_cursor.execute("write MySql query")
+my_cursor.execute(my_query)
 my_result = my_cursor.fetchone()
-my_results = my_cursor.fetchall()
+print(my_result)
+# my_results = my_cursor.fetchall()
+
