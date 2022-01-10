912bd212bf80468bc2a6f3ff050d9ce5b8fde68d
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-10 13:11:00 +0530

    files added

--- my_database.py
+++ my_database.py
@@ -9,10 +9,19 @@ WHERE points BETWEEN 1000 AND 3000
 ORDER BY points DESC
 LIMIT 1,1;
 """
-
+my_new_query = """
+SELECT * 
+FROM customers
+WHERE points BETWEEN 1000 AND 3000
+ORDER BY points DESC
+"""
 my_cursor = mydb.cursor()
 my_cursor.execute(my_query)
 my_result = my_cursor.fetchone()
 print(my_result)
-# my_results = my_cursor.fetchall()
+my_cursor.execute(my_new_query)
+my_new_result = my_cursor.fetchall()
+print(my_new_result)
+print(my_new_result[1][4])
+
 
