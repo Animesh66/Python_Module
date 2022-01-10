be08279c3a84cdf0ab6a1e8f73054e877e48a5b6
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-10 13:05:14 +0530

    files added

--- py_datetime.py
+++ py_datetime.py
@@ -5,7 +5,11 @@ epic_time = time()  # returns the time from origin of OS
 print(epic_time)
 print("No of Years: ", epic_time/31536000)
 dt2 = datetime.now()  # returns the current timestamp
-print(dt2)
+dt3 = datetime.today()
+dt4 = datetime.date(2018, 12, 11)
+print("dt2 is: ", dt2)
+print("dt3 is: ", dt3)
+print("dt4 is: ", dt4)
 print(dt2.strftime("%Y/%m/%d"))  # strftime method  converts date to string format
 dt1 = datetime.strptime("2018-12-31", "%Y-%m-%d") + timedelta(days=1)
 print(dt1)  # strptime method converts string to date format
