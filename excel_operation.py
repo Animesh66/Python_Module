2e42ccdca94e6da2bdb2f40a1126846fbea331f1
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-11 17:11:13 +0530

    files added

--- excel_operation.py
+++ excel_operation.py
@@ -3,10 +3,11 @@ import openpyxl as xl
 
 wb = xl.load_workbook("transactions.xlsx")
 sheet = wb["Sheet1"]
-cell = sheet.cell(row=1, column=1)
+cell = sheet.cell(row=2, column=2)
 rows = sheet.max_row
 columns = sheet.max_column
-print(cell.value)
+cell_value = cell.value
+print(cell_value)
 for row in range(2, rows +1):
     for column in range(1, columns + 1):
         cell_value = sheet.cell(row=row, column=column).value
