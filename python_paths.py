fdeba2066042eaeb4bd7dbd9639c3d856e0bce24
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-12 18:44:22 +0530

    files added

--- python_paths.py
+++ python_paths.py
@@ -6,6 +6,9 @@ path = Path("my_path/movies.json")
 source_file = path
 target_file = Path("New Location")
 shutil.copy(source_file, target_file)
+new_path = Path()
+for file in new_path.rglob("*.py"):  # recursively fine all the .py files as Generator object
+    print(file)
 print(path.exists())
 print(path.is_file())
 print(path.parent)
