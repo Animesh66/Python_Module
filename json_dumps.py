34a21643b4b2dbcbd95258f2f9ad86067cfa0276
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-19 11:43:03 +0530

    files added

--- json_dumps.py
+++ json_dumps.py
@@ -1,8 +1,10 @@
 import json
 from pathlib import Path
 
+new_list = ["1", "2", "3", "4"]
 movies = [{"id": 1, "name": "Terminator", "year": 1984}, {"id": 2, "name": "Harry Potter", "year": 2002}]
 
 data = json.dumps(movies)  # Converts Python readable data to JSON file
 path = Path("movies.json")
 path.write_text(data)  # Creates a JSON file named movies.json
+print("".join(new_list))
